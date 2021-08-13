cfd0f1b3fe9f99efe9167f378dc38619f78f0486
Brenden Vogt <bvogtdev@gmail.com>
2021-08-13 14:03:29 -0700

    Added user defaults check for version

--- Reminders/Views/MainView.swift
+++ Reminders/Views/MainView.swift
@@ -13,6 +13,27 @@ struct MainView: View {
     @State private var showNewReminderView = false
     @State private var showFeaturetteView = true
     
+    // Get current Version of the App
+    func getCurrentAppVersion() -> String {
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"]
+        let version = (appVersion as! String)
+        return version
+    }
+
+    // Check if app if app has been started after update
+    func checkForUpdate() {
+        let version = getCurrentAppVersion()
+        let savedVersion = UserDefaults.standard.string(forKey: "savedVersion")
+
+        if savedVersion == version {
+            print("App is up to date!")
+        } else {
+            // Toogle to show WhatsNew Screen as Modal
+            self.showFeaturetteView.toggle()
+            UserDefaults.standard.set(version, forKey: "savedVersion")
+        }
+    }
+
     var body: some View {
         NavigationView {
             ReminderView()
@@ -24,11 +45,13 @@ struct MainView: View {
                         }))
                 .sheet(isPresented: $showNewReminderView) {
                     NewReminderView().environmentObject(viewModel)
-                }.sheet(isPresented: $showFeaturetteView) {
+                }
+                .sheet(isPresented: $showFeaturetteView) {
                     FeaturetteView(show: $showFeaturetteView)
                 }
         }
         .navigationViewStyle(StackNavigationViewStyle())
+        .onAppear(perform: checkForUpdate)
     }
 }
 
